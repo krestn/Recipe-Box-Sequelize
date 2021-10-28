a6b0064bb66542e0dc2b55c0271547e15545622b
Kreston <kreston.cm@gmail.com>
2021-10-28 15:48:46 -0500

    Phase 10 fixed

--- models/measurementunit.js
+++ models/measurementunit.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
       name: {
         type: DataTypes.STRING,
         validate: {
-          notEmpty: true
+          notNull: true
         }
       },
     },
